6a1026bb9b1bc357cb131e212b4b7dd24f61fae0
RobbiAdam <100307218+RobbiAdam@users.noreply.github.com>
2024-04-07 13:56:32 +0900

    update room request

--- Booking.Client/DTOs/Requests/Rooms/CreateRoomRequest.cs
+++ Booking.Client/DTOs/Requests/Rooms/CreateRoomRequest.cs
@@ -1,9 +1,5 @@
 ﻿namespace Booking.Client.DTOs.Requests.Rooms
 {
-    public class CreateRoomRequest
-    {
-        public string Name { get; set; }
-        public int Capacity { get; set; }
-        public string Location { get; set; }
-    }
+    public record CreateRoomRequest(string Name, int Capacity, string Location)
+    
 }
--- Booking.Client/DTOs/Requests/Rooms/UpdateRoomRequest.cs
+++ Booking.Client/DTOs/Requests/Rooms/UpdateRoomRequest.cs
@@ -1,9 +1,5 @@
 ﻿namespace Booking.Client.DTOs.Requests.Rooms
 {
-    public class UpdateRoomRequest
-    {
-        public string Name { get; set; }
-        public int Capacity { get; set; }
-        public string Location { get; set; }
-    }
+    public record UpdateRoomRequest(string Name, int Capacity, string Location);
+
 }
