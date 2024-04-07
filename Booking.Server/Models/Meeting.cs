2c9a507be94b0eaf3f2da9d380ef7a4a84f15254
RobbiAdam <100307218+RobbiAdam@users.noreply.github.com>
2024-04-07 14:11:19 +0900

    update proj

--- Booking.Server/Models/Meeting.cs
+++ Booking.Server/Models/Meeting.cs
@@ -7,9 +7,12 @@ namespace Booking.Client.Models
         public string Title { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public string RoomId { get; set; }
+        public User Organizer { get; set; }
         public string UserId { get; set; }
-        public IEnumerable<User> Attendees { get; set; }
+        public Room Room { get; set; }
+        public string RoomId { get; set; }
+
+        public IEnumerable<User> Attendees { get; set; } = Enumerable.Empty<User>();
 
 
 
